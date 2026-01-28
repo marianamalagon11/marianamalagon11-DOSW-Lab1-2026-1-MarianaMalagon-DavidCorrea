a4d2051e2a169bb3d07abc0de0798e66831aa7ed
Shadday123 <shadday539@gmail.com>
2026-01-27 20:23:50 -0500

    Reto 5

---
+++ Laboratorio1/reto5/Reto5.java
@@ -0,0 +1,9 @@
+public class Reto5
+{
+    public static HashSet<Integer> almacenar(List<Integer> lista) {
+        HashSet<Integer> orden = lista.stream()
+                .filter(n -> n % 3 != 0)
+                .collect(Collectors.toCollection(HashSet::new));
+        return orden;
+    }
+}
