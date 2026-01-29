e91700179e6cd85546b309945d4762fb240757e2
Mariana <mariana.malagon-t@mail.escuelaing.edu.co>
2026-01-28 19:58:02 -0500

    Añadí el fragmento 2 del manual, reto 6

---
+++ Laboratorio1/reto6/MaquinaDecisiones.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaquinaDecisiones {
+    private static final Map<String, Runnable> comandos = new HashMap<>();
+
+    static {
+        comandos.put("BROMEAR", () -> System.out.println("La máquina ríe: ¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio..."));
+        comandos.put("GRITAR", () -> System.out.println("La máquina grita: ¡¡¡ALERTA DE STACK OVERFLOW!!!"));
+        comandos.put("SUSURRAR", () -> System.out.println("La máquina susurra: Shhh... los bugs están dormidos."));
+        comandos.put("ANALIZAR", () -> System.out.println("La máquina procesa: Analizando datos... resultado: ¡Eres increíble programando!"));
+    }
+
+    public static void manual(String comando) {
+        switch (comando) {
+            case "BROMEAR":
+            case "GRITAR":
+            case "SUSURRAR":
+            case "ANALIZAR":
+                comandos.get(comando).run();
+                break;
+            default:
+                System.out.println("comando incorrecto");
+                break;
+        }
+    }
+}
