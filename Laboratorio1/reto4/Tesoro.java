f592af265813cf7639e24f56bf574ad13af6def7
Mariana <mariana.malagon-t@mail.escuelaing.edu.co>
2026-01-28 19:21:36 -0500

    Añadí método de combinar y de ordenar e imprimir

--- Laboratorio1/reto4/Tesoro.java
+++ Laboratorio1/reto4/Tesoro.java
@@ -12,4 +12,19 @@ public class Tesoro{
                 .forEach(p -> tabla.put(p.getKey(), p.getValue()));
         return tabla;
     }
+
+    public static Hashtable<String, Integer> combinarHash(HashMap<String, Integer> mapa1, Hashtable<String, Integer> mapa2) {
+        Hashtable<String, Integer> combinado = new Hashtable<>();
+        mapa1.forEach((k, v) -> combinado.put(k, v));
+        mapa2.forEach((k, v) -> combinado.put(k, v));
+
+        return combinado;
+    }
+
+    public static void ordenoImprimo(Hashtable<String, Integer> mapa) {
+        mapa.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .forEach(e -> System.out.println("Clave: " + e.getKey() + " | Valor: " + e.getValue()));
+    }
 }
