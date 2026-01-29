ab9462247bd423086c27f295633082c1fcdb0293
Shadday123 <shadday539@gmail.com>
2026-01-28 22:10:36 -0500

    CarreraParalela paso 3

--- CarreraParalela.java
+++ CarreraParalela.java
@@ -1,1 +1,6 @@
-public class CarreraParalela{}
+public class CarreraParalela{
+
+    public int numeroMasGrande(List<Integer> lista) {
+        return lista.stream().max(Integer::compare).get();
+    }
+}
