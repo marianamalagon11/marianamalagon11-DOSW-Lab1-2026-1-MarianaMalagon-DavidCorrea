7b196586123e273958798f2c76d28f76cb44fb8c
Shadday123 <shadday539@gmail.com>
2026-01-28 21:29:12 -0500

    Merge reto5

--- Laboratorio1/reto6/MaquinaDecisiones.java
+++ Laboratorio1/reto6/MaquinaDecisiones.java
@@ -1,21 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MaquinaDecisiones {
 
-    public static String ejecutarComando(String comando) {
+    private static final Map<String, Runnable> comandos = new HashMap<>();
+
+    static {
+
+        comandos.put("BROMEAR", () -> System.out.println("La máquina ríe: ¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio..."));
+        comandos.put("GRITAR", () -> System.out.println("La máquina grita: ¡¡¡ALERTA DE STACK OVERFLOW!!!"));
+        comandos.put("SUSURRAR", () -> System.out.println("La máquina susurra: Shhh... los bugs están dormidos."));
+        comandos.put("ANALIZAR", () -> System.out.println("La máquina procesa: Analizando datos... resultado: ¡Eres increíble programando!"));
+        comandos.put("SALUDAR", () -> System.out.println("La máquina dice: ¡Saludos, viajero del Tiempo y del código!"));
+        comandos.put("DESPEDIR", () -> System.out.println("La máquina dice: Que los bits te acompañen, hasta la próxima misión."));
+        comandos.put("CANTAR", () -> System.out.println("La máquina canta: 01010101"));
+        comandos.put("DANZAR", () -> System.out.println("La máquina gira y emite chispas: Girando en modo fiesta."));
+    }
+
+    public static void ejecutarComando(String comando) {
         switch (comando) {
+            case "BROMEAR":
+            case "GRITAR":
+            case "SUSURRAR":
+            case "ANALIZAR":
             case "SALUDAR":
-                return "La máquina dice : ¡Saludos, viajero del Tiempo y del código!"";
-
             case "DESPEDIR":
-                return "La máquina dice: Que los bits te acompañen, hasta la próxima misión.";
-
             case "CANTAR":
-                return "La máquina canta :01010101";
-
             case "DANZAR":
-                return "La máquina gira y emite chispas: Girando en modo fiesta.";
-
+                comandos.get(comando).run();
+                break;
             default:
-                return "Comando desconocido.";
+                System.out.println("comando incorrecto");
+                break;
         }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        MaquinaDecisiones.ejecutarComando("SALUDAR");
+        MaquinaDecisiones.ejecutarComando("BROMEAR");
+        MaquinaDecisiones.ejecutarComando("ANALIZAR");
+        MaquinaDecisiones.ejecutarComando("DANZAR");
     }
 }
