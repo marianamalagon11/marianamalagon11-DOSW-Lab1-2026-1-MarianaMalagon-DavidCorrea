a2fbba91fd2aac25a057890573c3c38032c69ebd
Mariana <mariana.malagon-t@mail.escuelaing.edu.co>
2026-01-28 22:21:27 -0500

    Realicé el metodo para obtener el minimo y la cantidad de datos ingresados

--- CarreraParalela.java
+++ CarreraParalela.java
@@ -1,7 +1,16 @@
 public class CarreraParalela{
 
     public int numeroMasGrande(List<Integer> lista) {
-        return lista.stream().max(Integer::compare).get();
+        return lista.stream()
+                .max((a, b) -> a.compareTo(b))
+                .get();
+    }
+
+    public static void numPeqCant(List<Integer> lista) {
+        int minimo = lista.stream()
+                .min((a, b) -> a.compareTo(b))
+                .get();
+        int cantidad = lista.size();
     }
 
 }
