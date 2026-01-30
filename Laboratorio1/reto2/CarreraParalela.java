df4ee609924dcacf168051afc814299cee333cd2
Shadday123 <shadday539@gmail.com>
2026-01-30 10:31:50 -0500

    Reto2

---
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -0,0 +1,76 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class CarreraParalela {
+    private int minimo;
+    private int cantidad;
+    private int maximo;
+
+
+    public int numeroMasGrande(List<Integer> lista) {
+        return lista.stream()
+                .max((a, b) -> a.compareTo(b))
+                .get();
+    }
+
+
+    public static void numPeqCant(List<Integer> lista) {
+        int minimo = lista.stream()
+                .min((a, b) -> a.compareTo(b))
+                .get();
+        int cantidad = lista.size();
+    }
+
+
+    public static List<Integer> resultadosCombinados(List<Integer> lista) {
+        CarreraParalela obj = new CarreraParalela();
+        List<Integer> nuevaLista = new ArrayList<>();
+        int maximo = obj.numeroMasGrande(lista);
+        numPeqCant(lista); // Suma mínimo y cantidad
+        nuevaLista.add(maximo);
+        nuevaLista.add(obj.minimo);
+        nuevaLista.add(obj.cantidad);
+        return nuevaLista;
+    }
+
+
+    public static boolean divDeDos(int num) {
+        return (num % 2 == 0) ? true : false;
+    }
+
+
+    public static boolean datosImpares(int cantidad) {
+        return (cantidad % 2 != 0) ? true : false;
+    }
+
+    public static boolean verificadorCantDatosPar(List<Integer> lista){
+        return lista.size() % 2 == 0;
+    }
+
+
+    public List<Object> funcionFusionada(List<Integer> lista1, List<Integer> lista2) {
+        CarreraParalela obj = new CarreraParalela();
+
+
+        List<Integer> listaUnida = new ArrayList<>();
+        listaUnida.addAll(lista1);
+        listaUnida.addAll(lista2);
+
+
+        int numeroMaximo = obj.numeroMasGrande(listaUnida);
+        numPeqCant(listaUnida);
+
+
+        boolean maxEsMultiplo = divDeDos(numeroMaximo);
+        boolean cantEsPar = !datosImpares(listaUnida.size());
+
+        List<Object> resultados = new ArrayList<>();
+        resultados.add(numeroMaximo);
+        resultados.add(minimo);
+        resultados.add(listaUnida.size());
+        resultados.add(maxEsMultiplo);
+        resultados.add(cantEsPar);
+
+        return resultados;
+    }
+}
